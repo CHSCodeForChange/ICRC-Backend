2ecc188227390dbd3e26449aca4722fff0ecf5d2
Armaan Goel <armaangoel78@gmail.com>
2018-07-31 21:17:49 -0400

    initial, added form models

---
+++ complaint_form/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# from .forms import *
+from .models import *
+
+# Create your views here.
+
+
+# def fill_form(request):
+
+#     # if request.method == 'POST': 
+#     #     form = 
