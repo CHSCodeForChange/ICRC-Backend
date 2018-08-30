185c181a56d1d87ba32c1d7ea92f6d161fca0c81
Evan Kenyon <evan.kenyon123@gmail.com>
2018-08-30 17:36:43 -0400

    Finished PDF Creation for emails

--- complaint_form/views.py
+++ complaint_form/views.py
@@ -12,6 +12,8 @@ from django.contrib.sites.shortcuts import get_current_site
 
 from django.core import serializers
 
+from easy_pdf.rendering import render_to_pdf
+
 # Create your views here.
 
 
@@ -23,16 +25,30 @@ def fill_form(request):
         if form.is_valid():
 
             submitted = form.save()
+
+
+
             current_site = get_current_site(request)
+
             mail_subject = 'Form Submission from ' + submitted.first_name + " " + submitted.last_name
             message = render_to_string('email.html', {
                 'form': form,
                 'domain': current_site.domain,
+                'submitted': submitted,
             })
+
+            post_pdf = render_to_pdf('email.html', {
+                'form': form,
+                'domain': current_site.domain,
+                'submitted': submitted,
+            })
+
+
             to_email = 'icrcformtest@gmail.com'
             email = EmailMultiAlternatives(mail_subject, message, to=[to_email])
             email.content_subtype = 'html'
             email.mixed_subtype = 'related'
+            email.attach('' + submitted.first_name + '_' + submitted.last_name + '_complain_form.pdf', post_pdf, 'application/pdf')
             email.send()
             return redirect("/submitted")
 
