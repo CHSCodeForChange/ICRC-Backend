652a4c067655a28ecded2e3501160c3c775269a0
Evan Kenyon <evankenyon@Evans-MBP.attlocal.net>
2019-07-25 15:16:43 -0400

    productions file update

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icrc_backend.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icrc_backend.productions")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
