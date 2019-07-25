a344e9e54bf49d31c752398e0fbf279d44fc4d01
Evan Kenyon <evankenyon@Evans-MBP.attlocal.net>
2019-07-25 15:52:13 -0400

    Separating production and lolocal databases

--- icrc_backend/settings.py
+++ icrc_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'not secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "127.0.0.1"]
 
 
 # Application definition
@@ -145,5 +145,5 @@ PHONENUMBER_DEFAULT_REGION = 'US'
 EMAIL_HOST_USER = 'icrcformtest@gmail.com' #this is a testing account
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'Yqq-vuo-G7F-D6U'
+EMAIL_HOST_PASSWORD = 'pzblnoseqtypweah'
 EMAIL_PORT = 587
